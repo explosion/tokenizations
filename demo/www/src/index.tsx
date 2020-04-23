fb03b9cc8743949db1c52cf8539781bb96d055f8
Yohei Tamura <tamuhey@gmail.com>
2020-04-24 03:04:18 +0900

    ok

--- demo/www/src/index.tsx
+++ demo/www/src/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { ArcherContainer, ArcherElement } from "react-archer";
+import { GitHub } from "@material-ui/icons";
 import {
   Container,
   Paper,
@@ -10,6 +10,7 @@ import {
   CardContent,
   Typography,
   Box,
+  Link,
 } from "@material-ui/core";
 import LineTo from "react-lineto";
 
@@ -28,8 +29,10 @@ const tryParse = (input: string): [string[], boolean] => {
 };
 
 export const Index = () => {
-  const [inputA, setInputA] = useState('["fo","o","bar"]');
-  const [inputB, setInputB] = useState('["foo","bar"]');
+  const [inputA, setInputA] = useState(`["John", "Johanson", "'s", "house"]`);
+  const [inputB, setInputB] = useState(
+    `["john", "johan", "##son", "'", "s", "house"]`
+  );
   const [tokensA, errorA] = tryParse(inputA);
   const [tokensB, errorB] = tryParse(inputB);
   const [tokenization, setTokenization] = useState(null);
@@ -42,60 +45,73 @@ export const Index = () => {
     ? tokenization.get_alignment(tokensA, tokensB)
     : [[], []];
   console.log(a2b);
-  console.log(b2a);
   return (
-    <Container>
-      <Grid container>
-        <Grid item xs={12}>
-          <TextField
-            value={inputA}
-            onChange={(e) => setInputA(e.target.value)}
-            error={errorA}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            value={inputB}
-            onChange={(e) => setInputB(e.target.value)}
-            error={errorB}
-          />
-        </Grid>
-        <div className="tokens">
+    <Container maxWidth="md" style={{ marginTop: "20px" }}>
+      <Paper>
+        <Box display="flex" justifyContent="center" m={3} alignItems="center">
+          <Typography variant="h3">Tokenizations Demo</Typography>
+          <Link
+            href="https://github.com/tamuhey/tokenizations"
+            style={{ marginLeft: "20px" }}
+          >
+            <GitHub />
+          </Link>
+        </Box>
+        <Grid container spacing={3} style={{ padding: "30px" }}>
           <Grid item xs={12}>
-            <Box display="flex" bgcolor="background.paper" p={3} m={3}>
-              {tokensA.map((token, i) => (
-                <Box style={boxStyle} key={i} className={`a${i}`} m={1}>
-                  <Typography>{token}</Typography>
-                </Box>
-              ))}
-            </Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Box display="flex" bgcolor="background.paper" p={3} m={3}>
-              {tokensB.map((token, i) => {
-                return (
-                  <Box style={boxStyle} key={i} className={`b${i}`} m={1}>
-                    {token}
-                  </Box>
-                );
-              })}
-            </Box>
-          </Grid>
-        </div>
-        {a2b.map((l, i) => {
-          return l.map((j) => (
-            <LineTo
-              key={i * j}
-              from={`a${i}`}
-              to={`b${j}`}
-              zIndex={1}
-              fromAnchor="bottom"
-              toAnchor="top"
-              borderColor="black"
+            <TextField
+              value={inputA}
+              onChange={(e) => setInputA(e.target.value)}
+              error={errorA}
+              fullWidth
             />
-          ));
-        })}
-      </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              value={inputB}
+              onChange={(e) => setInputB(e.target.value)}
+              error={errorB}
+              fullWidth
+            />
+          </Grid>
+          <div className="tokens">
+            <Grid item xs={12}>
+              <Box display="flex" bgcolor="background.paper" p={3} m={3}>
+                {tokensA.map((token, i) => (
+                  <Box style={boxStyle} key={i} className={`a${i}`} m={1}>
+                    <Typography>{token}</Typography>
+                  </Box>
+                ))}
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Box display="flex" bgcolor="background.paper" p={3} m={3}>
+                {tokensB.map((token, i) => {
+                  return (
+                    <Box style={boxStyle} key={i} className={`b${i}`} m={1}>
+                      {token}
+                    </Box>
+                  );
+                })}
+              </Box>
+            </Grid>
+          </div>
+          {a2b.map((l, i) => {
+            return l.map((j) => (
+              <LineTo
+                delay={100}
+                key={`${i} ${j}`}
+                from={`a${i}`}
+                to={`b${j}`}
+                zIndex={1}
+                fromAnchor="bottom"
+                toAnchor="top"
+                borderColor="black"
+              />
+            ));
+          })}
+        </Grid>
+      </Paper>
     </Container>
   );
 };

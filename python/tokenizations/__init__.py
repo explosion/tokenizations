dbda0714f699d5cf48a32fc4a8c014de4c3ab468
Yohei Tamura <tamuhey@gmail.com>
2020-11-05 11:50:01 +0900

    add deprecate warning

--- python/tokenizations/__init__.py
+++ python/tokenizations/__init__.py
@@ -1,3 +1,18 @@
-from .tokenizations import get_alignments, get_charmap, get_original_spans
+import warnings
+from .tokenizations import (
+    get_alignments,
+    get_charmap,
+    get_original_spans as _get_original_spans,
+    __version__,
+)
 
-__all__ = ["get_charmap", "get_alignments", "get_original_spans"]
+
+def get_original_spans(tokens, original_text):
+    warnings.warn(
+        "get_original_spans is deprecated, use `textspan.get_original_spans` instead.",
+        DeprecationWarning,
+    )
+    return _get_original_spans(tokens, original_text)
+
+
+__all__ = ["get_charmap", "get_alignments", "get_original_spans", "__version__"]

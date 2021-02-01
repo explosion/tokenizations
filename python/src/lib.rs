4674b40a961cdd9f7af7b54df0f5e230972ab850
Yohei Tamura <tamuhey@gmail.com>
2021-02-02 00:27:14 +0900

    [versionup] 0.8.0 -> 0.8.1

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -4,7 +4,7 @@ use tokenizations::{get_alignments, get_charmap, Alignment, CharMap};
 
 #[pymodule]
 fn tokenizations(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add("__version__", "0.8.0")?;
+    m.add("__version__", "0.8.1")?;
 
     #[pyfn(m, "get_alignments")]
     pub fn get_alignments_py(

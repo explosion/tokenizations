f8b07b642a9317d3f8e49135b36c37c80f31d81a
Yohei Tamura <tamuhey@gmail.com>
2021-07-29 14:08:12 +0900

    update pyo3

--- python/tests/test_bench.py
+++ python/tests/test_bench.py
@@ -1,12 +1,15 @@
 """Benchmark"""
 import tokenizations
+import pytest
 
 
+@pytest.mark.benchmark(warmup=True, group="short", disable_gc=True)
 def test_short(benchmark):
     args = ["今日は", "\t", "いい", "天気だ", "。"], ["今日", "は", "いい", "天気", "た", "。"]
     benchmark(tokenizations.get_alignments, *args)
 
 
+@pytest.mark.benchmark(warmup=True, group="long", disable_gc=True)
 def test_long(benchmark):
     a = list("abcde") * 1000
     b = list("abbde") * 1000

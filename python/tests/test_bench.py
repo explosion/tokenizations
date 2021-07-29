bc84343b9ed4436ded22820616f491cef170944e
Yohei Tamura <tamuhey@gmail.com>
2021-07-29 13:35:52 +0900

    add benchmark

---
+++ python/tests/test_bench.py
@@ -0,0 +1,13 @@
+"""Benchmark"""
+import tokenizations
+
+
+def test_short(benchmark):
+    args = ["今日は", "\t", "いい", "天気だ", "。"], ["今日", "は", "いい", "天気", "た", "。"]
+    benchmark(tokenizations.get_alignments, *args)
+
+
+def test_long(benchmark):
+    a = list("abcde") * 1000
+    b = list("abbde") * 1000
+    benchmark(tokenizations.get_alignments, a, b)
